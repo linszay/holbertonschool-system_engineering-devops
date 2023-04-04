9955c254ffe8630b7f496774b83d275e862a66a0
linszay <lindseyjlancaster@gmail.com>
2023-04-03 20:38:51 -0500

    returns info about TODO list progress

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Retrieve and display an employee's TODO list progress"""
+
+import requests as req
+import sys
+
+
+if __name__ == "__main__":
+    # Check command line arguments
+    if len(sys.argv) != 2:
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Retrieve employee information
+    employee_id = sys.argv[1]
+    user_response = req.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(employee_id))
+    todo_response = req.get("https://jsonplaceholder.typicode.com/todos",
+                            params={"userId": employee_id})
+
+    # Check for request errors
+    if user_response.status_code != 200:
+        print("Error: User ID not found")
+        sys.exit(1)
+    if todo_response.status_code != 200:
+        print("Error: Could not retrieve TODO list")
+        sys.exit(1)
+
+    # Parse JSON responses
+    user_data = user_response.json()
+    todo_data = todo_response.json()
+
+    # Extract relevant data
+    employee_name = user_data["name"]
+    total_tasks = len(todo_data)
+    done_tasks = sum(1 for task in todo_data if task["completed"])
+    completed_titles = [task["title"]
+                        for task in todo_data if task["completed"]]
+
+    # Display TODO list progress
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          done_tasks, total_tasks))
+    for title in completed_titles:
+        print("\t {}".format(title))
