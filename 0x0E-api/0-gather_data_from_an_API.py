b24be57a75d1ac670769df23f46f4e5ddf61e29d
linszay <lindseyjlancaster@gmail.com>
2023-04-03 22:04:15 -0500

    modified file for the checker

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,41 +1,42 @@
 #!/usr/bin/python3
 """Retrieve and display an employee's TODO list progress"""
 import requests
-from sys import argv
+from sys import argv, exit
 
 
-if __name__ == '__main__':
+def get_employee_todo_progress(employee_id):
+    """gets employee progress via their employee ID"""
+    baseurl = "https://jsonplaceholder.typicode.com"
+
+    user_resp = requests.get("{}/users/{}".format(baseurl, employee_id))
+    userdata = user_resp.json()
+
+    if 'name' not in userdata:
+        print("Invalid employee ID")
+        return
+
+    todo_resp = requests.get("{}/users/{}/todos".format(baseurl, employee_id))
+    tododata = todo_resp.json()
+
+    completedtasks = [task for task in tododata if task["completed"]]
+    totaltasks = len(tododata)
+
+    print("Employee {} is done with tasks({}/{}): "
+          .format(userdata['name'], len(completedtasks), totaltasks))
+
+    for task in completedtasks:
+        print("\t", task["title"])
+
+
+if __name__ == "__main__":
     if len(argv) != 2:
-        print("Usage: {} employee_id".format(argv[0]))
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
         exit(1)
 
-    employee_id = argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-        employee_id)
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        print("Error: Request failed with status code {}".format(
-            response.status_code))
+    try:
+        employee_id = int(argv[1])
+    except ValueError:
+        print("Employee ID must be an integer")
         exit(1)
 
-    todos = response.json()
-    total_tasks = len(todos)
-    done_tasks = [todo for todo in todos if todo['completed']]
-
-    employee_name_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id)
-    response_name = requests.get(employee_name_url)
-
-    if response_name.status_code != 200:
-        print("Error: Request failed with status code {}".format(
-            response_name.status_code))
-        exit(1)
-
-    employee_name = response_name.json().get('name')
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, len(done_tasks), total_tasks))
-
-    for todo in done_tasks:
-        print("\t {} {}".format('\t', todo['title']))
+    get_employee_todo_progress(employee_id)
