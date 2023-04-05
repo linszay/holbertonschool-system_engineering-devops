3386ba0419b448f537e317335b83bd171960e254
linszay <lindseyjlancaster@gmail.com>
2023-04-04 19:35:25 -0500

    updating file permissions

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Retrieve and display an employee's TODO list progress"""
+"""retrieve and display an employee's TODO list progress"""
 
 import requests as req
 import sys
@@ -18,19 +18,27 @@ if __name__ == "__main__":
     todo_response = req.get("https://jsonplaceholder.typicode.com/todos",
                             params={"userId": employee_id})
 
+    # Check for request errors
+    if user_response.status_code != 200:
+        print("Error: User ID not found")
+        sys.exit(1)
+    if todo_response.status_code != 200:
+        print("Error: Could not retrieve TODO list")
+        sys.exit(1)
+
     # Parse JSON responses
     user_data = user_response.json()
     todo_data = todo_response.json()
 
     # Extract relevant data
-    employee_name = user_data['name']
+    employee_name = user_data["name"]
     total_tasks = len(todo_data)
-    done_tasks = sum(1 for task in todo_data if task['completed'])
-    completed_titles = [task['title']
-                        for task in todo_data if task['completed']]
+    done_tasks = sum(1 for task in todo_data if task["completed"])
+    completed_titles = [task["title"]
+                        for task in todo_data if task["completed"]]
 
     # Display TODO list progress
     print("Employee {} is done with tasks({}/{}):".format(employee_name,
           done_tasks, total_tasks))
     for title in completed_titles:
-        print("\t {}".format(task['title']))
+        print("\t {}".format(title))
